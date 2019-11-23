9e8d7b6989cb4130bf57be400eb1309acd292f81
Pashio84 <19fmi03@ms.dendai.ac.jp>
2019-11-24 00:13:24 +0900

    Collect crude oil price and exchange

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,12 @@ global.createNewFile = (): void => {
 };
 
 global.updateSheet = (): void => {
+  const apikey = '';
   let regularGasolinePrice: number;
   let highOctaneGasolinePrice: number;
   let wtiCrudeOilPrice: number;
   let brentCrudeOilPrice: number;
+  let exchange: number;
   let temperature: number;
   let precipitation: number;
 
@@ -19,12 +21,42 @@ global.updateSheet = (): void => {
   let regularGasolineDom = document.match(
     /<label>レギュラー<\/label>\s*<div class="price">([0-9\.])*<\/div>/
   )[0];
-  regularGasolinePrice = Number(regularGasolineDom.replace(/<\/?[a-z]*( [a-z\=\"]*)?>[ア-ンー]*(\s *)?/g, ''));
+  regularGasolinePrice = getInnerNumber(regularGasolineDom);
   Logger.log(`Regular = ${regularGasolinePrice}`);
 
   let highOctaneGasolineDom = document.match(
     /<label>ハイオク<\/label>\s*<div class="price">([0-9\.])*<\/div>/
   )[0];
-  highOctaneGasolinePrice = Number(highOctaneGasolineDom.replace(/<\/?[a-z]*( [a-z\=\"]*)?>[ア-ンー]*(\s *)?/g, ''));
+  highOctaneGasolinePrice = getInnerNumber(highOctaneGasolineDom);
   Logger.log(`High octane = ${highOctaneGasolinePrice}`);
+
+  document = fetchFromPhantomJs('https://nikkei225jp.com/oil/');
+  let wtiCrudeOilDom = document.match(
+    /<div id="V921" class="val2 valN">[\s ]*<p>[0-9.]*<\/p>[\s ]*<\/div>/
+  )[0];
+  wtiCrudeOilPrice = getInnerNumber(wtiCrudeOilDom);
+  Logger.log(`WTI crude oil = ${wtiCrudeOilPrice}`);
+
+  let brentCrudeOilDom = document.match(
+    /<div id="V922" class="val2 valN">[\s ]*<p>[0-9.]*<\/p>[\s ]*<\/div>/
+  )[0];
+  brentCrudeOilPrice = getInnerNumber(brentCrudeOilDom);
+  Logger.log(`Brent crude oil = ${brentCrudeOilPrice}`);
+
+  let exchangeDom = document.match(
+    /<div id="V511" class="val2">[\s ]*<p class="smy">[0-9.]*<\/p>[\s ]*<\/div>/
+  )[0];
+  exchange = getInnerNumber(exchangeDom);
+  Logger.log(`Exchange = ${exchange}`);
+
+  function getInnerNumber(dom: string): number {
+    return Number(dom.replace(/\ *<\/?[a-zA-Z0-9\=\"\ ]*>[ア-ンー]*[\s\ ]*?/g, ''));
+  }
+
+  function fetchFromPhantomJs(target: string): string {
+    var url = `https://phantomjscloud.com/api/browser/v2/${apikey}/?request=%7Burl:%22${target}%22,renderType:%22HTML%22,outputAsJson:true%7D`;
+    var response = UrlFetchApp.fetch(url).getContentText();
+    var json = JSON.parse(response);
+    return json['content']['data'].toString();
+  }
 };
