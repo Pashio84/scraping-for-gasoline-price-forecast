fbfdbc1dcf5970482fd20d7d0ba61f86866bc057
Pashio84 <19fmi03@ms.dendai.ac.jp>
2019-11-23 22:57:09 +0900

    Collect gasoline price

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,25 @@ global.createNewFile = (): void => {
   const ss = SheetService.createInitialFile('New file');
   ss.getRange('A2').setValue('Happy gas!');
 };
+
+global.updateSheet = (): void => {
+  let regularGasolinePrice: number;
+  let highOctaneGasolinePrice: number;
+  let wtiCrudeOilPrice: number;
+  let brentCrudeOilPrice: number;
+  let temperature: number;
+  let precipitation: number;
+
+  let document = UrlFetchApp.fetch('https://gogo.gs/12').getContentText('UTF-8');
+  let regularGasolineDom = document.match(
+    /<label>レギュラー<\/label>\s*<div class="price">([0-9\.])*<\/div>/
+  )[0];
+  regularGasolinePrice = Number(regularGasolineDom.replace(/<\/?[a-z]*( [a-z\=\"]*)?>[ア-ンー]*(\s *)?/g, ''));
+  Logger.log(`Regular = ${regularGasolinePrice}`);
+
+  let highOctaneGasolineDom = document.match(
+    /<label>ハイオク<\/label>\s*<div class="price">([0-9\.])*<\/div>/
+  )[0];
+  highOctaneGasolinePrice = Number(highOctaneGasolineDom.replace(/<\/?[a-z]*( [a-z\=\"]*)?>[ア-ンー]*(\s *)?/g, ''));
+  Logger.log(`High octane = ${highOctaneGasolinePrice}`);
+};
